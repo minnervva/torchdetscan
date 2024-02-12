47536078a0805ee0676976d4bdfeecf2cbb6d14f
may <colettima@ornl.gov>
2024-02-11 22:59:09 -0500

    Added start of setup.py

---
+++ minnervva/setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup
+
+setup(name='MINNERVVA', version='0.0', packages=[''], url='', license='',
+        author='Ada Sedova, Mark Coletti, Wael Elwasif', author_email='',
+        description='')
