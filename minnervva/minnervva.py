3e9931c1a2bbda6486e41f43f9a8e624cec42853
may <colettima@ornl.gov>
2024-02-12 00:06:06 -0500

    Hooking up verbose flag

--- minnervva/minnervva.py
+++ minnervva/minnervva.py
@@ -3,6 +3,7 @@
 
 Linter for finding non-deterministic functions in pytorch code.
 
+usage: minnervva.py [-h] [--verbose VERBOSE] path
 """
 import argparse
 from pathlib import Path
@@ -16,8 +17,8 @@ MINNERVA is a linter for finding non-deterministic functions in pytorch code.
 def find_function_calls(node):
     """ Recursively find all function calls in a node.
 
-    :param node: The node to search for function calls.
-    :returns: A list of all function calls in the node.
+        :param node: The node to search for function calls.
+        :returns: A list of all function calls in the node.
     """
     function_calls = []
     for child in ast.iter_child_nodes(node):
@@ -28,13 +29,16 @@ def find_function_calls(node):
     return function_calls
 
 
-def lint_file(path: Path):
+def lint_file(path: Path, verbose: bool = False):
     """Lint a single file.
 
     :param path: The path to the file to lint.
+    :param verbose: Whether to enable chatty output.
     :returns: None
     """
-    print(f'Linting file: {args.path}')
+    if verbose:
+        print(f'Linting file: {args.path}')
+
     with open(path, 'r') as file:
         source = file.read()
 
@@ -55,11 +59,12 @@ def main():
     args = parser.parse_args()
 
     if args.path.is_file():
-        lint_file(args.path)
+        lint_file(args.path, args.verbose)
     elif args.path.is_dir():
-        print(f'Linting directory: {args.path}')
+        if args.verbose:
+            print(f'Linting directory: {args.path}')
         for file in args.path.rglob('*.py'):
-            lint_file(file)
+            lint_file(file, args.verbose)
     else:
         print(f'Path does not exist: {args.path}')
 
