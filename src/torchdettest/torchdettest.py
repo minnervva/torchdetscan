c55f82a68df9da139b5be4b0fb3bd74710613a93
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2024-09-19 16:27:35 -0400

    Add input for function name or csv file path

--- src/torchdettest/torchdettest.py
+++ src/torchdettest/torchdettest.py
@@ -1,16 +1,41 @@
-#!/usr/bin/env python3
-"""
-    For testing pytorch functions for non-deterministic vs. deterministic
-    behavior.
-"""
+"""Test pytorch functions for non-deterministic vs. deterministic behavior."""
+
 import argparse
 
 # TODO add guts of non-deterministic testing here
 
-if __name__ == '__main__':
+
+def main():
+    """Command line interface for torchdettest.
+
+    Run from command line and provide a function name:
+    python src/torchdettest/torchdettest.py ConvTranspose1d
+
+    Run from command line and provide path to CSV file:
+    python src/torchdettest/torchdettest.py /user/home/myfuncs.csv
+    """
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('--verbose', type=bool, default=False,
-                        help='Print verbose output')
+
+    parser.add_argument(
+        "--verbose", type=bool, default=False, help="Print verbose output"
+    )
+
+    parser.add_argument(
+        "function", type=str, help="Provide function name or path to CSV file"
+    )
+
     args = parser.parse_args()
 
-    pass
+    function: str = args.function
+
+    if function.endswith(".csv"):
+        # The input argument is a path to a CSV file
+        # The CSV file contains a list of function names
+        print(f"Path to CSV file is {function}")
+    else:
+        # The input argument is the name of a function
+        print(f"Function name is {function}")
+
+
+if __name__ == "__main__":
+    main()
