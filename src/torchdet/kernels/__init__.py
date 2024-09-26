3cabbe1522d96f908a885dfed264adec98925010
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2024-09-26 14:43:49 -0400

    Import functions and classes from kernels subpackage

---
+++ src/torchdet/kernels/__init__.py
@@ -0,0 +1,37 @@
+"""Kernels subpackage."""
+
+from .benchmark import nn_benchmark, func_benchmark
+from .avg_pool import AvgPoolLoop, BatchDimLoop, avg_pool_loop
+from .conv import ConvLoop, convolution_loop
+from .scatter import ScatterLoop, ScatterDimLoop, scatter_loop
+from .scatter_reduce import ScatterReduceLoop, ScatterReduceDimLoop, scatter_reduce_loop
+from .gather import GatherLoop, GatherDimLoop, gather_loop
+from .index_add import IndexAddLoop, IndexAddDimLoop, index_add_loop
+from .index_copy import index_copy_loop
+from .index_put import IndexPutLoop, IndexPutDimLoop, index_put_loop
+
+__all__ = [
+    "nn_benchmark",
+    "func_benchmark",
+    "AvgPoolLoop",
+    "BatchDimLoop",
+    "avg_pool_loop",
+    "ConvLoop",
+    "convolution_loop",
+    "ScatterLoop",
+    "ScatterDimLoop",
+    "scatter_loop",
+    "ScatterReduceLoop",
+    "ScatterReduceDimLoop",
+    "scatter_reduce_loop",
+    "GatherLoop",
+    "GatherDimLoop",
+    "gather_loop",
+    "IndexAddLoop",
+    "IndexAddDimLoop",
+    "index_add_loop",
+    "index_copy_loop",
+    "IndexPutLoop",
+    "IndexPutDimLoop",
+    "index_put_loop"
+]
